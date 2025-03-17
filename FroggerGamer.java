97f7977aa109b7e5531a08bf7b4b32cec13f0a8c
JereMP4 <jeremythieblemont0@icloud.com>
2025-03-17 16:55:10 +0100

    Retrait des .class et sépration de la classe Obstacle

--- FroggerGamer.java
+++ FroggerGamer.java
@@ -1,41 +1,5 @@
 import java.util.*;
 
-
-class Obstacle extends Thread {
-    private int x;
-    private int y;
-    private boolean active = true;
-    public static final int WIDTH = 10;
-    private static final int HEIGHT = 10;
-    public static final String OBSTACLE_CHAR = "🚣";
-    public static final String FINISH_LINE_CHAR = "🏁";
-    
-    public Obstacle(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-    
-    public void run() {
-        Random rand = new Random();
-        while (active) {
-            try {
-                Thread.sleep(rand.nextInt(1000) + 500);
-                moveObstacle();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    
-    private void moveObstacle() {
-        x = (x + 1) % WIDTH;
-    }
-    
-    public int getX() { return x; }
-    public int getY() { return y; }
-    public String getChar() { return OBSTACLE_CHAR; }
-}
-
 public class FroggerGamer {
     public static final String FINISH_LINE_CHAR = "🏁";
     public static final String WALL_CHAR = "🧱";
