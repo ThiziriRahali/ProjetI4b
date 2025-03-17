09c991aa18e4bd1894776cbc94d0f853c4054321
ThiziriRahali <thizirirahali2@gmail.com>
2025-03-17 10:16:03 +0100

    Remise à la version de base

--- FroggerGamer.java
+++ FroggerGamer.java
@@ -1,11 +1,9 @@
 import java.util.Random;
 
-// Ceci est un test
 
 class Obstacle extends Thread {
     private int x;
     private int y;
-    private boolean isTest = False;
     private boolean active = true;
     private static final int WIDTH = 10;
     private static final int HEIGHT = 10;
